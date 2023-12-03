963ff30ad1e22b2634127aeb3e97b05fe8e147a4
kladarak <scottjenkins88@gmail.com>
2023-12-03 09:17:59 +0100

    Day03: Tidied up
    - Pulled out parsing of data into Schematic struct
    - Parts 01 and 02 rewritten to be simple accumulates over the parsed data.

--- cpp/day03.cpp
+++ cpp/day03.cpp
@@ -8,89 +8,6 @@
 #include <unordered_map>
 #include <vector>
 
-static std::vector<std::string> loadSchematic(const char* filename)
-{
-	std::fstream s{ filename, s.in };
-	assert(s.is_open());
-
-	std::vector<std::string> schematic;
-	std::optional<size_t> width;
-
-	while (s.peek() != EOF)
-	{
-		std::string line;
-		std::getline(s, line);
-
-		assert(line.size() > 0);
-		assert(line.back() != '\n');
-
-		schematic.push_back(line);
-
-		if (width)
-			assert(line.size() == width.value());
-		else
-			width = line.size();
-	}
-
-	return schematic;
-}
-
-static uint64_t partOne(const std::vector<std::string>& schematic)
-{
-	const size_t width = schematic.front().size();
-
-	auto hasSymbolNeighbour = [&] (const size_t row, size_t col, size_t mag)
-	{
-		bool foundSymbol = false;
-
-		for (size_t x = col == 0 ? col : col - 1;
-			x < std::min(col + mag + 1, width);
-			++x)
-		{
-			for (size_t y = row == 0 ? row : row - 1;
-				y < std::min(row + 2, schematic.size());
-				++y)
-			{
-				const char c = schematic[y][x];
-				foundSymbol |= !std::isdigit(c) && c != '.';
-			}
-		}
-
-		return foundSymbol;
-	};
-
-	uint64_t sum = 0;
-
-	for (size_t row = 0; row < schematic.size(); ++row)
-	{
-		for (size_t col = 0; col < width; ++col)
-		{
-			char c = schematic[row][col];
-			if (!std::isdigit(c))
-				continue;
-
-			int num = 0;
-			size_t mag = 0;
-
-			for (; col < width; ++col)
-			{
-				c = schematic[row][col];
-				if (!std::isdigit(c))
-					break;
-
-				num *= 10;
-				num += c - '0';
-				++mag;
-			}
-
-			if (hasSymbolNeighbour(row, col - mag, mag))
-				sum += num;
-		}
-	}
-
-	return sum;
-}
-
 struct Symbol
 {
 	char symbol = 0;
@@ -106,75 +23,148 @@ struct Part
 	size_t y = 0;
 };
 
-static uint64_t partTwo([[maybe_unused]]  const std::vector<std::string>& schematic)
+struct Gear
 {
-	const size_t width = schematic.front().size();
+	const Part* part1 = nullptr;
+	const Part* part2 = nullptr;
+	const Symbol* symbol = nullptr;
+
+	uint64_t ratio() const { return part1->value * part2->value; }
+};
+
+struct Schematic
+{
+	std::vector<std::string> data;
+	size_t width = 0;
+	size_t height = 0;
 
 	std::vector<Part> parts;
-	std::vector<std::vector<Symbol>> symbolsPerRow;
-	symbolsPerRow.resize(schematic.size());
+	std::vector<Symbol> symbols;
+	std::vector<Gear> gears;
 
-	for (size_t row = 0; row < schematic.size(); ++row)
-	{
-		for (size_t col = 0; col < width; ++col)
-		{
-			const char c = schematic[row][col];
-			if (std::isdigit(c))
-			{
-				const uint64_t partNum = std::stoull(schematic[row].substr(col));
-				parts.push_back({ partNum, 0, col, row });
-
-				while (col < width && std::isdigit(schematic[row][col]))
-				{
-					++parts.back().mag;
-					++col;
-				}
-
-				--col;
-			}
-			else if (c != '.')
-			{
-				symbolsPerRow[row].push_back({c, col, row});
-			}
-		}
-	}
-
+	std::vector<const Part*> partsNeighbouringSymbols;
 	std::unordered_map<const Symbol*, std::vector<const Part*>> symbolPartMap;
 
-	for (const Part& part : parts)
+	explicit Schematic(const char* filename)
 	{
-		const size_t minX = part.x == 0 ? part.x : part.x - 1;
-		const size_t maxX = std::min(part.x + part.mag, width - 1);
-		const size_t minY = part.y == 0 ? part.y : part.y - 1;
-		const size_t maxY = std::min(part.y + 1, schematic.size() - 1);
+		loadData(filename);
+		identifyPartsAndSynbols();
+		buildPartSymbolMap();
+		identifyGears();
+	}
 
-		for (size_t y = minY; y <= maxY; ++y)
+	void loadData(const char* filename)
+	{
+		std::fstream s{ filename, s.in };
+		assert(s.is_open());
+
+		while (s.peek() != EOF)
 		{
-			for (const Symbol& symbol : symbolsPerRow[y])
+			std::string line;
+			std::getline(s, line);
+
+			assert(line.size() > 0);
+			assert(line.back() != '\n');
+
+			data.push_back(line);
+
+			if (width != 0)
+				assert(line.size() == width);
+			else
+				width = line.size();
+		}
+
+		height = data.size();
+	}
+	
+	void identifyPartsAndSynbols()
+	{
+		for (size_t row = 0; row < height; ++row)
+		{
+			for (size_t col = 0; col < width; ++col)
 			{
-				if (minX <= symbol.x && symbol.x <= maxX)
-					symbolPartMap[&symbol].push_back(&part);
+				const char c = data[row][col];
+				if (std::isdigit(c))
+				{
+					const uint64_t partNum = std::stoull(data[row].substr(col));
+					parts.push_back({ partNum, 0, col, row });
+
+					while (col < width && std::isdigit(data[row][col]))
+					{
+						++parts.back().mag;
+						++col;
+					}
+
+					--col;
+				}
+				else if (c != '.')
+				{
+					symbols.push_back({ c, col, row });
+				}
 			}
 		}
 	}
 
-	uint64_t sum = 0;
-
-	for (auto pair : symbolPartMap)
+	void buildPartSymbolMap()
 	{
-		if (pair.first->symbol == '*' && pair.second.size() == 2)
+		std::vector<std::vector<Symbol*>> symbolsPerRow;
+		symbolsPerRow.resize(height);
+
+		for (Symbol& s : symbols)
+			symbolsPerRow[s.y].push_back(&s);
+
+		for (const Part& part : parts)
 		{
-			const uint64_t product = pair.second[0]->value * pair.second[1]->value;
-			sum += product;
+			const size_t minX = part.x == 0 ? part.x : part.x - 1;
+			const size_t maxX = std::min(part.x + part.mag, width - 1);
+			const size_t minY = part.y == 0 ? part.y : part.y - 1;
+			const size_t maxY = std::min(part.y + 1, height - 1);
+
+			for (size_t y = minY; y <= maxY; ++y)
+			{
+				for (const Symbol* symbol : symbolsPerRow[y])
+				{
+					if (minX <= symbol->x && symbol->x <= maxX)
+					{
+						partsNeighbouringSymbols.push_back(&part);
+						symbolPartMap[symbol].push_back(&part);
+					}
+				}
+			}
 		}
 	}
 
-	return sum;
+	void identifyGears()
+	{
+		for (auto pair : symbolPartMap)
+			if (pair.first->symbol == '*' && pair.second.size() == 2)
+				gears.push_back({ pair.second[0], pair.second[1], pair.first });
+	}
+};
+
+static uint64_t partOne(const Schematic& schematic)
+{
+	return std::accumulate(
+		begin(schematic.partsNeighbouringSymbols),
+		end(schematic.partsNeighbouringSymbols),
+		0ull,
+		[] (uint64_t sum, const Part* part) { return sum + part->value; }
+	);
+}
+
+static uint64_t partTwo(const Schematic& schematic)
+{
+	return std::accumulate(
+		begin(schematic.gears),
+		end(schematic.gears),
+		0ull,
+		[] (uint64_t sum, const Gear& gear) { return sum + gear.ratio(); }
+	);
 }
 
 static std::pair<uint64_t, uint64_t> process(const char* filename)
 {
-	const auto schematic = loadSchematic(filename);
+	const Schematic schematic(filename);
 	return std::make_pair(partOne(schematic), partTwo(schematic));
 }
 
@@ -189,5 +179,5 @@ static void processPrintAndAssert(const char* filename, std::optional<std::pair<
 void day03()
 {
 	processPrintAndAssert("../data/03/test.txt", std::make_pair(4361ull, 467835ull));
-	processPrintAndAssert("../data/03/real.txt", std::make_pair(539713ull, 0ull));
+	processPrintAndAssert("../data/03/real.txt", std::make_pair(539713ull, 84159075ull));
 }
